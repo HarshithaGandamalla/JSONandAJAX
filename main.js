c5e80a4197c19997bbc789279f8715b4545b6fb6
Harshitha <harshithag15@gmail.com>
2018-04-24 15:28:04 -0400

    Completed AJAX request to URL

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 
 ourRequest.onload = function(){
-    console.log(ourRequest.responseText);
+   
+//    var ourData = ourRequest.responseText; //giant text string response
+    var ourData = JSON.parse(ourRequest.responseText); //Read string text as JSON
+    
+    console.log(ourData[0]);
+    
+    
+    
 };
 
 ourRequest.send();

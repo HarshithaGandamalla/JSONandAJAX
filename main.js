784977e15e2caccad4270cdc293c77b4dd8bb79e
Harshitha <harshithag15@gmail.com>
2018-04-24 16:51:28 -0400

    Added error handling

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-var ourRequest = new XMLHttpRequest();
+var pageCounter =1;
+var btn = document.getElementById('btn');
+var animalContainer  = document.getElementById('animal-info');
 
-ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 
-ourRequest.onload = function(){
+btn.addEventListener("click",function(){
    
-//    var ourData = ourRequest.responseText; //giant text string response
-    var ourData = JSON.parse(ourRequest.responseText); //Read string text as JSON
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-'+pageCounter+'.json');
+    ourRequest.onload = function(){
+    //var ourData = ourRequest.responseText; //giant text string response
+    if(ourRequest.status>=200 && ourRequest.status<400){    
+    renderHTML(ourRequest.responseText);
+    }else{
+        console.log("Connected to server, but error receiveing data from server");
+    }
+         
     
-    console.log(ourData[0]);
-    
-    
-    
-};
+    };
 
-ourRequest.send();
+    ourRequest.send(); //sending our request 
+    pageCounter++;
+    if(pageCounter>3)
+        btn.classList.add("hide-me");
+});
+
+function renderHTML(data){
+    var ourData = JSON.parse(data); //Read string text as JSON
+    var htmlString ="";
+    for(var i=0;i<ourData.length;i++){
+        htmlString += "<p>"+ourData[i].name+" is a "+ourData[i].species+ ".</p>";
+    }
+        
+    animalContainer.insertAdjacentHTML('beforeend',htmlString);  
+}
+
